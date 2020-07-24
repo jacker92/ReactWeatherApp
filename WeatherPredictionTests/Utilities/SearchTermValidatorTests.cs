e363a7396e68aed486dba361527fdbc6e4495c78
Jaakko Lahtinen <lahtinen.jaakko@gmail.com>
2020-07-24 10:24:17 +0300

    Added SearchTermValidatorTests to solution

---
+++ WeatherPredictionTests/Utilities/SearchTermValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WeatherPrediction.Utilities;
+
+namespace WeatherPredictionTests.Utilities
+{
+    [TestClass]
+    public class SearchTermValidatorTests
+    {
+        private SearchTermValidator _searchTermValidator;
+
+        [TestInitialize]
+        public void InitTests()
+        {
+            _searchTermValidator = new SearchTermValidator();
+        }   
+
+        [TestMethod]
+        public void SearchTermValidator_ShouldReturnTrueForValidStrings(string searchTerm)
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SearchTermValidator_ShouldReturnFalseForInvalidStrings(string searchTerm)
+        {
+            Assert.Fail();
+        }
+    }
+}
