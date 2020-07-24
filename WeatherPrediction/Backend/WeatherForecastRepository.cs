78334c1f7e7c66b68208950a4c33031b8838b73a
Jaakko Lahtinen <lahtinen.jaakko@gmail.com>
2020-07-24 10:15:48 +0300

    Started building cache to WeatherForecastRepository.

--- WeatherPrediction/Backend/WeatherForecastRepository.cs
+++ WeatherPrediction/Backend/WeatherForecastRepository.cs
@@ -13,18 +13,25 @@ namespace WeatherPrediction.Backend
     public class WeatherForecastRepository : IRepository<WeatherForecastModel>
     {
         private readonly IConfiguration _configuration;
+        private readonly Dictionary<string, IEnumerable<WeatherForecastModel>> _weatherForecastModelCache;
 
         public WeatherForecastRepository(IConfiguration configuration)
         {
             _configuration = configuration;
+            _weatherForecastModelCache = new Dictionary<string, IEnumerable<WeatherForecastModel>>();
         }
         public IEnumerable<WeatherForecastModel> GetItems(string searchString)
         {
+            if(_weatherForecastModelCache.ContainsKey(searchString))
+            {
+                return _weatherForecastModelCache[searchString];
+            }
+
             using (var client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Add("X-RapidAPI-Host", _configuration["X-RapidAPI-Host"]);
                 client.DefaultRequestHeaders.Add("X-RapidAPI-Key", _configuration["X-RapidAPI-Key"]);
-                var jsonResponse = client.GetStringAsync(@"https://community-open-weather-map.p.rapidapi.com/find&q=" + searchString).Result;
+                var jsonResponse = client.GetStringAsync(@"https://community-open-weather-map.p.rapidapi.com/find?type=link%252C%20accurate&units=imperial%252C%20metric&q=" + searchString).Result;
 
                 return WeatherForecastModelBuilder.Build(jsonResponse);
             }
