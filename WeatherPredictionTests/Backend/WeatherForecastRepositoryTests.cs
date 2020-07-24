a06176886ecbdd4658f0f9f10ca61ed6c17a1d59
Jaakko Lahtinen <lahtinen.jaakko@gmail.com>
2020-07-24 10:09:31 +0300

    Added Test project to solution.

---
+++ WeatherPredictionTests/Backend/WeatherForecastRepositoryTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeatherPrediction.Backend;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using System.Linq;
+
+namespace WeatherPrediction.Backend.Tests
+{
+    [TestClass()]
+    public class WeatherForecastRepositoryTests
+    {
+        private Mock<IRepository<WeatherForecastModel>> _weatherForecastRepository;
+
+        [TestInitialize]
+        public void InitTests()
+        {
+            _weatherForecastRepository = new Mock<IRepository<WeatherForecastModel>>();
+        }
+
+        [TestMethod()]
+        [DataRow("test")]
+        public void GetWeatherForecastModels_ShouldReturnItemsWhenCalled(string searchString)
+        {
+            _weatherForecastRepository.Setup(x => x.GetItems(searchString)).Returns(new List<WeatherForecastModel>()
+           { new WeatherForecastModel() 
+            
+            });
+
+            var result = _weatherForecastRepository.Object.GetItems(searchString);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IEnumerable<WeatherForecastModel>));
+            Assert.AreEqual(1, result.Count());
+        }
+    }
+}
